414547c2ea39d962202189a6afdc76c8a1070441
mwhitey <mwhitey17@gmail.com>
2016-12-02 13:00:47 -0500

    validation

--- server/src/server.js
+++ server/src/server.js
@@ -4,12 +4,85 @@ var bodyParser = require('body-parser');
 // Imports the express Node module.
 var reverseMod = require('./util');
 var reverseString = reverseMod.reverseString;
-var read = require('./database')
-var readDocument = read.readDocument;
+var database = require('./database')
+var readDocument = database.readDocument;
+var StatusUpdateSchema = require('./schemas/statusupdate.json');
 
+var validate = require('express-jsonschema').validate;
+var writeDocument = database.writeDocument;
+var addDocument = database.addDocument;
+var express = require('express');
+// Creates an Express server.
+var app = express();
+// Support receiving text in HTTP request bodies
+app.use(bodyParser.text());
+// Support receiving JSON in HTTP request bodies
+app.use(bodyParser.json());
 /**
 * Resolves a feed item. Internal to the server, since it's synchronous.
 */
+/**
+/**
+* Adds a new status update to the database.
+*/
+function postStatusUpdate(user, location, contents) {
+// If we were implementing this for real on an actual server, we would check
+// that the user ID is correct & matches the authenticated user. But since
+// we're mocking it, we can be less strict.
+// Get the current UNIX time.
+var time = new Date().getTime();
+// The new status update. The database will assign the ID for us.
+var newStatusUpdate = {
+"likeCounter": [],
+"type": "statusUpdate",
+"contents": {
+"author": user,
+"postDate": time,
+"location": location,
+"contents": contents,
+"likeCounter": []
+},
+// List of comments on the post
+"comments": []
+};
+// Add the status update to the database.
+// Returns the status update w/ an ID assigned.
+newStatusUpdate = addDocument('feedItems', newStatusUpdate);
+// Add the status update reference to the front of the current user's feed.
+var userData = readDocument('users', user);
+var feedData = readDocument('feeds', userData.feed);
+feedData.contents.unshift(newStatusUpdate._id);
+// Update the feed object.
+writeDocument('feeds', feedData);
+// Return the newly-posted object.
+return newStatusUpdate;
+}
+// `POST /feeditem { userId: user, location: location, contents: contents }`
+app.post('/feeditem',
+validate({ body: StatusUpdateSchema }), function(req, res) {
+// If this function runs, `req.body` passed JSON validation!
+var body = req.body;
+var fromUser = getUserIdFromToken(req.get('Authorization'));
+// Check if requester is authorized to post this status update.
+// (The requester must be the author of the update.)
+if (fromUser === body.userId) {
+var newUpdate = postStatusUpdate(body.userId, body.location,
+body.contents);
+// When POST creates a new resource, we should tell the client about it
+// in the 'Location' header and use status code 201.
+res.status(201);
+res.set('Location', '/feeditem/' + newUpdate._id);
+// Send the update!
+res.send(newUpdate);
+} else {
+// 401: Unauthorized.
+res.status(401).end();
+}
+});
+
+
+
+
 function getFeedItemSync(feedItemId) {
 var feedItem = readDocument('feedItems', feedItemId);
 // Resolve 'like' counter.
@@ -39,13 +112,14 @@ feedData.contents = feedData.contents.map(getFeedItemSync);
 return feedData;
 }
 
-var express = require('express');
-// Creates an Express server.
-var app = express();
+
+
+
+
 // You run the server from `server`, so `../client/build` is `server/../client/build`.
 // '..' means "go up one directory", so this translates into `client/build`!
 app.use(express.static('../client/build'));
-app.use(bodyParser.text());
+
 // Handle POST /reverse [data]
 // app.post('/reverse', function (req, res) {
 // // If the request came with text, then the text() middleware handled it
@@ -114,6 +188,18 @@ res.status(401).end();
 }
 });
 
+/**
+* Translate JSON Schema Validation failures into error 400s.
+*/
+app.use(function(err, req, res, next) {
+if (err.name === 'JsonSchemaValidation') {
+// Set a bad request http response status
+res.status(400).end();
+} else {
+// It's some other sort of error; pass it to next error middleware handler
+next(err);
+}
+});
 
 app.listen(3000, function () {
 console.log('Example app listening on port 3000!');
